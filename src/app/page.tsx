9d5188e4ba23df22d81ebcf2f299d19eef4e7162
Ilkka <ilkka.ratilainen@gmail.com>
2026-06-12 09:00:30 +0300

    feat: Add tamplate of StatusBar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 // src/app/page.tsx
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import StatusBar from '@/components/StatusBar';
 
 export default function MainPage() {
-  console.log("MainPage-component loaded correctly");
+  useEffect(() => {
+    console.log("MainPage-component loaded correctly");
+    console.log("StatusBar is visible");
+  }, []);
 
   return (
     <div>
-      VOID COLLECTOR
+      <h1>VOID COLLECTOR</h1>
+      <StatusBar />
     </div>
   );
 }
--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,1 +1,16 @@
 // components/StatusBar
+
+'use client';
+
+import React from "react";
+
+export default function StatusBar() {
+    console.log("StatusBar-component loaded correctly");
+
+    return (
+        <div>
+            <h2>Status Bar</h2>
+            <p>This is StatusBar-component</p>
+        </div>
+    );
+}
